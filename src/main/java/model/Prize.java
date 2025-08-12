4c332df8e682defb05df4d2f0c2071056fad3cb5
Donghun <2dh2@naver.com>
2025-08-12 15:39:29 +0900

    feat. 사다리 게임 상품 관련 클래스 생성

---
+++ src/main/java/model/Prize.java
@@ -0,0 +1,25 @@
+package model;
+
+import exception.CustomException;
+import exception.ErrorMessage;
+
+public class Prize {
+
+    private final String prize;
+
+    public Prize(String prize) {
+        validate(prize);
+
+        this.prize = prize;
+    }
+
+    private void validate(String prize) {
+        if (prize == null || prize.isBlank()) {
+            throw new CustomException(ErrorMessage.PRIZE_EMTPY);
+        }
+    }
+
+    public String getPrize() {
+        return prize;
+    }
+}
