ddfa60334464802ee5cb11d3dfb1d908802c79a6
Donghun <2dh2@naver.com>
2025-08-12 16:03:48 +0900

    feat. 전체 플레이어 결과, 단일 플레이어 결과 출력하는 메소드 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,7 +9,7 @@ import model.Prizes;
 public class OutputView {
 
     public void printExecuteResult(Ladder ladder, Players players, Prizes prizes) {
-        System.out.println("실행결과 \n");
+        System.out.println("\n사다리결과\n");
 
         for (Player player : players.getPlayers()) {
             System.out.printf("%-6s", player.getName());
@@ -20,13 +20,32 @@ public class OutputView {
         ladder.print();
 
         for (Prize prize : prizes.getPrizes()) {
-            System.out.printf("%-6s ", prize.getPrize());
+            System.out.printf("%-4s ", prize.getPrize());
         }
 
+        System.out.println("\n");
+    }
+
+    public void printAllPlayerResult(Ladder ladder, Players players, Prizes prizes) {
         System.out.println();
 
-        for (int i = 0; i < ladder.getLines().size() - 1; i++) {
-            System.out.printf("%d -> %d \n", i, ladder.move(i));
+        for (int i = 0; i < players.size(); i++) {
+            String playerName = players.getPlayers().get(i).getName();
+            int position = ladder.move(i);
+            String prize = prizes.getPrizes().get(position).getPrize();
+
+            System.out.printf("%s : %s\n", playerName, prize);
         }
+    }
+
+    public void printSinglePlayerResult(Ladder ladder, Players players, Prizes prizes, String playerName) {
+        System.out.println();
+
+        int index = players.indexOf(playerName);
+        int position = ladder.move(index);
+        String prize = prizes.getPrizes().get(position).getPrize();
+
+        System.out.println("실행결과");
+        System.out.println(prize);
     }
 }
