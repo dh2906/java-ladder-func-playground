826d4904c185caa0d38870aa3b25db1b698553c9
Donghun <2dh2@naver.com>
2025-08-12 13:15:23 +0900

    feat. 입력 메소드 적용

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -1,23 +1,31 @@
 package controller;
 
 import java.util.Random;
+import java.util.Scanner;
 
+import model.Height;
 import model.Ladder;
 import model.LadderGenerator;
+import model.Width;
+import view.InputView;
 import view.OutputView;
 
 public class LadderGameController {
 
     private final LadderGenerator ladderGenerator;
     private final OutputView outputView;
+    private final InputView inputView;
 
     public LadderGameController() {
         this.ladderGenerator = new LadderGenerator(new Random());
         this.outputView = new OutputView();
+        this.inputView = new InputView(new Scanner(System.in));
     }
 
     public void run() {
-        Ladder ladder = ladderGenerator.generate(4, 4);
+        Width width = inputView.inputWidth();
+        Height height = inputView.inputHeight();
+        Ladder ladder = ladderGenerator.generate(height, width);
 
         outputView.printExecuteResult(ladder);
     }
