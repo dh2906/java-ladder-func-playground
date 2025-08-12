47e70173b9e06be2a7e81d7ef923f40903a2bce3
Donghun <2dh2@naver.com>
2025-08-12 15:40:16 +0900

    fix. 사다리 결과 출력 시 List의 Index를 벗어나는 문제 해결

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,15 +1,31 @@
 package view;
 
 import model.Ladder;
-import model.Line;
+import model.Player;
+import model.Players;
+import model.Prize;
+import model.Prizes;
 
 public class OutputView {
 
-    public void printExecuteResult(Ladder ladder) {
+    public void printExecuteResult(Ladder ladder, Players players, Prizes prizes) {
         System.out.println("실행결과 \n");
+
+        for (Player player : players.getPlayers()) {
+            System.out.printf("%-6s", player.getName());
+        }
+
+        System.out.println();
+
         ladder.print();
 
-        for (int i = 0; i < ladder.getLines().size(); i++) {
+        for (Prize prize : prizes.getPrizes()) {
+            System.out.printf("%-6s ", prize.getPrize());
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < ladder.getLines().size() - 1; i++) {
             System.out.printf("%d -> %d \n", i, ladder.move(i));
         }
     }
