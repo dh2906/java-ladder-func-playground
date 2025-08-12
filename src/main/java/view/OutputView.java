50bdc72e193d3a8c4ec678acffdf9f739a9cc7b5
Donghun <2dh2@naver.com>
2025-08-12 14:45:28 +0900

    feat. 사다리 타기 결과 출력 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,11 +1,16 @@
 package view;
 
 import model.Ladder;
+import model.Line;
 
 public class OutputView {
 
     public void printExecuteResult(Ladder ladder) {
         System.out.println("실행결과 \n");
         ladder.print();
+
+        for (int i = 0; i < ladder.getLines().size(); i++) {
+            System.out.printf("%d -> %d \n", i, ladder.move(i));
+        }
     }
 }
