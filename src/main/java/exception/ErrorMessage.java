1e47ba4ff4e27ced60619d6fa11e636450729ae2
Donghun <2dh2@naver.com>
2025-08-12 16:52:38 +0900

    feat. 에러 메시지 추가

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -9,7 +9,8 @@ public enum ErrorMessage {
     PLAYER_NAMES_EMPTY("참여자가 존재하지 않습니다"),
     PRIZE_EMPTY("당첨 결과가 비어있습니다."),
     PRIZES_EMPTY("당첨 결과 리스트가 비어있습니다."),
-    PLAYER_NOT_FOUND("해당 플레이어를 찾을 수 없습니다.");
+    PLAYER_NOT_FOUND("해당 플레이어를 찾을 수 없습니다."),
+    NULL_INPUT_STRING("입력 문자열로 null이 입력 되었습니다.");
 
     private final String message;
 
